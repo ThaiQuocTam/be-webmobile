6120c6f0c4edf8f2a01185d83e36912ef9f2e0a8
ThaiQuocTam <thaiquoctam123@gmail.com>
2023-07-10 09:25:57 +0700

    update config connect DB

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,10 +1,14 @@
 const { Sequelize, Model } = require("sequelize")
 
-const sequelize = new Sequelize("postgres", "postgres", "a@Anhtam01234253554", {
-    host: process.env.DB_HOST,
-    port: "5432",
-    dialect: "postgres"
-})
+const sequelize = new Sequelize(
+    process.env.DB_DATABASE_NAME,
+    process.env.DB_USERNAME,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: "postgres"
+    })
 
 const connectDB = async () => {
     try {
